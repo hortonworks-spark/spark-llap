0fe293fd65427046ff963788b6454a15c83f115b
Dongjoon Hyun <dongjoon@apache.org>
2017-03-03 11:43:47 -0800

    [SPARK-LLAP-71] Fix branch-1.6 with the known issues found in branch-2.1

--- src/main/scala/org/apache/spark/sql/hive/llap/LlapContext.scala
+++ src/main/scala/org/apache/spark/sql/hive/llap/LlapContext.scala
@@ -17,15 +17,21 @@
 
 package org.apache.spark.sql.hive.llap
 
-import scala.reflect.runtime.{universe => ru}
+import java.io.PrintStream
 import java.sql.Connection
+import java.util.{Map => JMap}
+
+import scala.reflect.runtime.{universe => ru}
+
 import com.hortonworks.spark.sql.hive.llap.DefaultJDBCWrapper
+
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLConf
 import org.apache.spark.sql.SQLConf.SQLConfEntry.stringConf
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.analysis.OverrideCatalog
+import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.plans.logical.Subquery
 import org.apache.spark.sql.execution.CacheManager
@@ -35,8 +41,7 @@ import org.apache.spark.sql.execution.ui.SQLListener
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.hive.HiveMetastoreCatalog
 import org.apache.spark.sql.hive.MetastoreRelation
-import org.apache.spark.sql.hive.client.ClientInterface
-import org.apache.spark.sql.hive.client.ClientWrapper
+import org.apache.spark.sql.hive.client.{ClientInterface, ClientWrapper, HiveDatabase, HiveTable}
 
 class LlapContext(sc: SparkContext,
     cacheManager: CacheManager,
@@ -91,8 +96,63 @@ class LlapContext(sc: SparkContext,
   }
 }
 
-class LlapCatalog(override val client: ClientInterface, hive: LlapContext)
-    extends HiveMetastoreCatalog(client, hive) {
+private[hive] class ClientProxy(client: ClientInterface) extends ClientInterface {
+  // scalastyle:off
+  def version() = client.version
+  def getConf(key: String, defaultValue: String) = client.getConf(key, defaultValue)
+  def runSqlHive(sql: String) = runSqlHive(sql)
+  def setOut(stream: PrintStream) = client.setOut(stream)
+  def setInfo(stream: PrintStream) = client.setInfo(stream)
+  def setError(stream: PrintStream) = client.setError(stream)
+  def listTables(dbName: String) = client.listTables(dbName)
+  def currentDatabase = client.currentDatabase
+  override def getDatabase(name: String) = client.getDatabase(name)
+  def getDatabaseOption(name: String) = client.getDatabaseOption(name)
+  override def getTable(dbName: String, tableName: String) = client.getTable(dbName, tableName)
+  def getTableOption(dbName: String, tableName: String) = client.getTableOption(dbName, tableName)
+  def createView(view: HiveTable) = client.createView(view)
+  def alertView(view: HiveTable) = client.alertView(view)
+  def createTable(table: HiveTable) = client.createTable(table)
+  def alterTable(table: HiveTable) = client.alterTable(table)
+  def createDatabase(database: HiveDatabase) = client.createDatabase(database)
+  def getPartitionOption(hTable: HiveTable, partitionSpec: JMap[String, String]) =
+    client.getPartitionOption(hTable, partitionSpec)
+  def getAllPartitions(hTable: HiveTable) = client.getAllPartitions(hTable)
+  def getPartitionsByFilter(hTable: HiveTable, predicates: Seq[Expression]) =
+    client.getPartitionsByFilter(hTable, predicates)
+  def loadPartition(
+      loadPath: String,
+      tableName: String,
+      partSpec: java.util.LinkedHashMap[String, String], // Hive relies on LinkedHashMap ordering
+      replace: Boolean,
+      holdDDLTime: Boolean,
+      inheritTableSpecs: Boolean,
+      isSkewedStoreAsSubdir: Boolean) =
+    client.loadPartition(
+      loadPath, tableName, partSpec, replace, holdDDLTime, inheritTableSpecs, isSkewedStoreAsSubdir)
+  def loadTable(loadPath: String, tableName: String, replace: Boolean, holdDDLTime: Boolean) =
+    client.loadTable(loadPath, tableName, replace, holdDDLTime)
+  def loadDynamicPartitions(
+      loadPath: String,
+      tableName: String,
+      partSpec: java.util.LinkedHashMap[String, String], // Hive relies on LinkedHashMap ordering
+      replace: Boolean,
+      numDP: Int,
+      holdDDLTime: Boolean,
+      listBucketingEnabled: Boolean) =
+    client.loadDynamicPartitions(
+      loadPath, tableName, partSpec, replace, numDP, holdDDLTime, listBucketingEnabled)
+  def addJar(path: String) = client.addJar(path)
+  def newSession() = client.newSession()
+  def withHiveState[A](f: => A): A = client.withHiveState[A](f)
+  def reset() = client.reset()
+  // scalastyle:on
+}
+
+class LlapCatalog(val client2: ClientInterface, hive: LlapContext)
+    extends HiveMetastoreCatalog(client2, hive) {
+
+  override val client = client2
 
   override def lookupRelation(
       tableIdentifier: TableIdentifier,
