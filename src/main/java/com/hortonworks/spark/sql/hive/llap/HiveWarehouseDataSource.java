53b1871b68faf7bae6b5d9c8b13a8c1fb7ca77cc
Eric Wohlstadter <ewohlstadter@hortonworks.com>
2018-04-13 21:00:17 -0700

    Refactoring. Fix option input for hs2 url and dbcp2.conf. Remove unused code

--- src/main/java/com/hortonworks/spark/sql/hive/llap/HiveWarehouseDataSource.java
+++ src/main/java/com/hortonworks/spark/sql/hive/llap/HiveWarehouseDataSource.java
@@ -19,30 +19,30 @@ public class HiveWarehouseDataSource implements DataSourceV2, ReadSupport, Sessi
     private static final String HIVE_WAREHOUSE_PREFIX = "hive.warehouse";
     private static Logger LOG = LoggerFactory.getLogger(HiveWarehouseDataSource.class);
 
-        @Override
-        public DataSourceReader createReader(DataSourceOptions options) {
-            try {
-                SparkSession session = SparkSession.getActiveSession().get();
-                if(session == null) {
-                    throw new IllegalStateException("Must have active SparkSession to use HiveWarehouse");
-                }
-                Map<String, String> params = new HashMap<>();
-                String connectionUrl = options.get("url").get();
-                params.put("query", options.get("query").get());
-                params.put("table", options.get("table").get());
-                //TODO username/password
-                params.put("user.name", "");
-                params.put("user.password", "");
-                //TODO handle dbcp.conf
-                params.put("dbcp2.conf", null);
-                params.put("url", connectionUrl);
-                return new HiveWarehouseDataSourceReader(params);
-            } catch (IOException e) {
-                LOG.error("Error creating {}", getClass().getName());
-                LOG.error(ExceptionUtils.getStackTrace(e));
+    @Override
+    public DataSourceReader createReader(DataSourceOptions options) {
+        try {
+            SparkSession session = SparkSession.getActiveSession().get();
+            if(session == null) {
+                throw new IllegalStateException("Must have active SparkSession to use HiveWarehouse");
             }
-            return null;
+            Map<String, String> params = new HashMap<>();
+            String connectionUrl = options.get("url").get();
+            params.put("query", options.get("query").get());
+            params.put("table", options.get("table").get());
+            //TODO username/password
+            params.put("user.name", "");
+            params.put("user.password", "");
+            //TODO handle dbcp.conf
+            params.put("dbcp2.conf", null);
+            params.put("url", connectionUrl);
+            return new HiveWarehouseDataSourceReader(params);
+        } catch (IOException e) {
+            LOG.error("Error creating {}", getClass().getName());
+            LOG.error(ExceptionUtils.getStackTrace(e));
         }
+        return null;
+    }
 
     @Override
     public String keyPrefix() {
