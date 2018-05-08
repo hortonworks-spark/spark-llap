9cb3da7882adc204d432847184c79cb1556759df
ewohlstad <wohlstad@gmail.com>
2018-05-08 07:12:18 -0700

    [SPARK-LLAP-207] Add missing apache headers. Change default hs2 port to real default (#211)
    ## What changes were proposed in this pull request?
    
    Add missing apache headers from previous PR. Change default hs2 port in HWConf to real hs2 default port.
    
    ## How was this patch tested?
    
    Check that build still succeeds

--- src/main/java/com/hortonworks/spark/sql/hive/llap/HWConf.java
+++ src/main/java/com/hortonworks/spark/sql/hive/llap/HWConf.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hortonworks.spark.sql.hive.llap;
 
 import java.util.Optional;
@@ -6,7 +23,7 @@ enum HWConf {
 
   USER("user.name", warehouseKey("user.name"), ""),
   PASSWORD("password", warehouseKey("password"), ""),
-  HS2_URL("hs2.url", warehouseKey("hs2.url"), "jdbc:hive2://localhost:10084"),
+  HS2_URL("hs2.url", warehouseKey("hs2.url"), "jdbc:hive2://localhost:10500"),
   DBCP2_CONF("dbcp2.conf", warehouseKey("dbcp2.conf"), null),
   DEFAULT_DB("default.db", warehouseKey("default.db"), "default"),
   MAX_EXEC_RESULTS("exec.results.max", warehouseKey("exec.results.max"), 1000L);
