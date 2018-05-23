11db6ceef311df433e849e048fc68bc64ac2007a
Eric Wohlstadter <ewohlstadter@hortonworks.com>
2018-05-22 22:09:23 -0700

    Add .count() impl

--- src/main/scala/com/hortonworks/spark/sql/hive/llap/DataSourceV2CountStrategy.scala
+++ src/main/scala/com/hortonworks/spark/sql/hive/llap/DataSourceV2CountStrategy.scala
@@ -1,5 +1,17 @@
 package com.hortonworks.spark.sql.hive.llap
 
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.expressions.aggregate.{AggregateExpression, Count}
+import org.apache.spark.sql.catalyst.{FunctionIdentifier, TableIdentifier}
+import org.apache.spark.sql.catalyst.expressions.{Alias, Expression}
+import org.apache.spark.sql.catalyst.parser.{CatalystSqlParser, ParserInterface}
+import org.apache.spark.sql.catalyst.plans.logical.{Aggregate, LogicalPlan}
+import org.apache.spark.sql.catalyst.rules.Rule
+import org.apache.spark.sql.execution.datasources.v2.DataSourceV2Relation
+import org.apache.spark.sql.execution.{SparkPlan, SparkStrategy}
+import org.apache.spark.sql.types.{DataType, StructType}
+
+
 case class DataSourceV2CountStrategy(spark: SparkSession) extends Rule[LogicalPlan] {
   override def apply(plan: LogicalPlan): LogicalPlan = plan match {
     case Aggregate(groupingExpressions,
